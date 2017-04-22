f93661ca91f039d5168ef957786940bb8d5b80d2
Trenton Potgieter <darkreapyre@gmail.com>
2017-04-22 10:19:22 -0700

    updated typos

--- src/ukf.cpp
+++ src/ukf.cpp
@@ -330,9 +330,9 @@ void UKF::UpdateLidar(MeasurementPackage meas_package) {
     VectorXd x_diff = Xsig_pred_.col(i) - x_;
     //angle normalization
     while (x_diff(3) > M_PI) x_diff(3) -= 2. * M_PI;
-    while (x_diff(3) <- M_PI) x_diff(3)) += 2. * M_PI;
+    while (x_diff(3) <- M_PI) x_diff(3) += 2. * M_PI;
 
-    Tc = Tc + weights(i) * x_diff * z_diff.transpose();
+    Tc = Tc + weights_(i) * x_diff * z_diff.transpose();
   }
 
   //calculate Kalman gain K
