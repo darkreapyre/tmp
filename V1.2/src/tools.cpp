dc7601bbafc987fdc879b3cd1b9e5d79d523bfa8
Trenton Potgieter <darkreapyre@gmail.com>
2017-04-23 10:56:14 -0700

    test tool

--- V1.2/src/tools.cpp
+++ V1.2/src/tools.cpp
@@ -12,30 +12,30 @@ Tools::~Tools() {}
 VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
                               const vector<VectorXd> &ground_truth) {
 
-    int est_size = estimations.size();
-    vector<VectorXd> residuals;
+    int n = estimations.size();
+//    vector<VectorXd> residuals;
     VectorXd rmse(4);
     rmse << 0,0,0,0;
-    VectorXd mean(4);
+//    VectorXd mean(4);
 
-    //  * the estimation vector size should not be zero
-    //  * the estimation vector size should equal ground truth vector size
-    if (est_size == 0) {
-        std::cout << "Estimantions vector size is 0" << std::endl;
-        return rmse;
-    } else if (est_size != ground_truth.size()) {
-        std::cout << "Estimantions vector size is not equal to Ground Truth vector size" << std::endl;
-        return rmse;
-    }
+//    //  * the estimation vector size should not be zero
+//    //  * the estimation vector size should equal ground truth vector size
+//    if (est_size == 0) {
+//        std::cout << "Estimantions vector size is 0" << std::endl;
+//        return rmse;
+//    } else if (est_size != ground_truth.size()) {
+//        std::cout << "Estimantions vector size is not equal to Ground Truth vector size" << std::endl;
+//        return rmse;
+//    }
 
     //accumulate squared residuals
-    for(int i=0; i < est_size; ++i){
+    for(int i = 0; i < n; ++i){
         VectorXd residual = estimations[i] - ground_truth[i];
         residual = residual.array() * residual.array();
         rmse = rmse.array() + residual.array();
     }
     //calculate the mean
-    rmse = rmse / est_size;
+    rmse = rmse / n;
     //calculate the squared root
     rmse = rmse.array().sqrt();
 
